bae07e3e31d87165746ebb5391c1525bc1e23b3c
brassyk <brass.99.skyagh@gmail.com>
2022-10-10 21:41:24 +0900

    modified:   src/common/lib/works.ts

--- src/common/lib/works.ts
+++ src/common/lib/works.ts
@@ -33,7 +33,7 @@ export const works: worksArray = [
     image: '/assets/works/camera-kaitorisatei.jpeg',
     description:
       '<p>担当業務: 運用保守 バナー制作</p>カメラ買取系企業のLPの運用保守を主に担当。また、サイト内バナーを2パターン*2サイズ制作。<br/>Google Spread Sheetと連携した出力機能を実装。クライアントに直接スプレッドシート内に入力してもらうことで、買取実績の更新を簡素化に成功。</p>',
-    categories: ['1', '4', '6'],
+    categories: ['1', '15', '21'],
   },
   {
     id: 4,
@@ -41,7 +41,7 @@ export const works: worksArray = [
     image: '/assets/works/car-buy-and-sell.jpg',
     description:
       '<p>想定した対象: 30代 ~ 50代男性。外車を所有。</p><p>高級感と親しみやすさの両方のリクエスト。チャコールを基調とした色調と見出し背景に大理石のテクスチャを用いることで落ち着いた印象を持たせつつ、見出しに用いるフォントをややカジュアルで力強いものにすることで両立を目指した。</p><p>ロゴはネオン感のあるものを希望されたため、ネオンサインをイメージしつつ、直線と曲線を組み合わせたタイプで車を表現。</p><p>Google Spread Sheetと連携した出力機能を実装。クライアントに直接スプレッドシート内に入力してもらうことで、買取実績の更新を簡素化に成功。</p>',
-    categories: ['4'],
+    categories: ['1', '5', '13'],
   },
   {
     id: 5,
@@ -49,7 +49,7 @@ export const works: worksArray = [
     image: '/assets/works/ad-llc.jpeg',
     description:
       '<p>担当業務: デザイン、コーディング、ロゴデザイン</p><p>必要最低限のコンテンツと機能のみを希望。ジュエリーや時計等、高級な嗜好品を取り扱うため、色調と見出しフォントをシックな印象で統一した。</p>',
-    categories: ['4'],
+    categories: ['1', '12'],
     link: 'https://gt-asset-management.asia'
   },
   {
@@ -58,7 +58,7 @@ export const works: worksArray = [
     image: '/assets/works/asia-super.jpg',
     description:
       '<p>担当業務: カスタマイズ、店舗情報・商品・コレクション・タグ登録、決済システム導入、在庫管理システム・POS連携、スタッフへのサポート</p><p>食品・飲料、雑貨を販売する実店舗のECサイトをShopifyで構築。Shopify CLIで構築。スライダー、商品一覧の"カートに追加"ボタン、ポップアップ、問い合わせ機能を実装。</p>',
-    categories: ['4'],
+    categories: ['1', '12', '14'],
     link: 'https://www.asia-super.com/'
   },
   {
@@ -67,7 +67,7 @@ export const works: worksArray = [
     image: '/assets/works/iemon2_300x250.jpg',
     description:
       '<p>担当業務: バナー制作</p><p>リースバックのWeb広告用バナー。2パターン*2サイズを制作。<br/>企業ロゴに合わせて緑を基調とした近似色でまとめた。</p><p>提供された文章をまとめ、目に止まりやすいよう努めた。</p>',
-    categories: ['2'],
+    categories: ['2', '21'],
   },
   {
     id: 8,
@@ -75,7 +75,7 @@ export const works: worksArray = [
     image: '/assets/works/cocodaUI1.jpg',
     description:
       '<p>Cocoda! UIカリキュラム#001 SNSのプロフィール画面</p><p>ペルソナ、コンセプト: プライベートも楽しむ働き盛りの26才女性。生活の中で撮る写真をオシャレに見せたい。</p><p>Neumorphismを使ったInstagram風の画像投稿メイン型SNS<br/>パステルやモノトーンなど、様々なユーザのテイストに合わせられるように明るくニュートラルなグレーで配色。フォローボタンのみをブルーにすることでハイライトさせる。<br/>保存した画像の表示ページを下の固定に設置。ネイルサロンやヘアサロンなどでの「こんな風にしてください」や、買い物中の「あれって何のブランド(商品名)だっけ?」が解決しやすくなり、ユーザの利用時間を増やせる。<br/>igは「縦(の画像)に弱い」ことが弱点とされていたので、19:6、4:3、1:1の画像をグリッドで表示できるフィードに。グリッドに合わないと感じたので、Pinterestのような角丸は使わず。</p><p><a href="https://xd.adobe.com/view/7fb5cbf0-8763-47e3-8356-e1e018d652c8-2ae6/">アニメーション(Adobe XD)</p>',
-    categories: ['2'],
+    categories: ['2', '23'],
     link: 'https://xd.adobe.com/view/7fb5cbf0-8763-47e3-8356-e1e018d652c8-2ae6/'
   },
   {
@@ -92,7 +92,7 @@ export const works: worksArray = [
     image: '/assets/works/cocodaDaily06.jpeg',
     description:
       '<p>Cocoda! Daily Day6 LIPSコスメプレゼント企画のクリエイティブ</p><p>ペルソナ、コンセプト: LIPSユーザ = 10代から20代前半。プレゼントするコスメはデパコス。</p><p>読みやすさと明朝体の高級感を両立するため、余白を設け、漫画のセリフで主に使われるアンチック体とゴシック体の組み合わせた源暎アンチックを使用。</p>',
-    categories: ['2'],
+    categories: ['2', '21'],
   },
   {
     id: 11,
@@ -100,6 +100,6 @@ export const works: worksArray = [
     image: '/assets/works/cocodaDaily08.jpeg',
     description:
       '<p>Cocoda! Daily Day8 ヴィーガン向け料理レシピサイトのロゴ</p><p>ペルソナ、コンセプト: 10代~20代女性。ヴィーガン、ナチュラル思考。新しいレシピを「発見」する。</p><p>野菜、自然をイメージさせる色調を使用。c"oo"kの部分を目に見立て、"新しいレシピを「発見」する"ことを表現。</p>',
-    categories: ['3'],
+    categories: ['3', '22'],
   },
 ]
